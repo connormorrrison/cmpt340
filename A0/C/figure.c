f5e7e86fb656842552e635893df7ddaa940e276b
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-01-10 02:37:08 -0600

    Initial project structure setup and Makefile setup
    - Set up initial project structure with C and Java subdirectories
    - Initial Makefile implementation
    - Basic  main.c, figure.c and figure.h files

---
+++ A0/C/figure.c
@@ -0,0 +1,7 @@
+/* TODO */
+
+static int placeholder = 0;
+
+void use_placeholder(void) {
+    placeholder = 0;
+}
---
+++ A0/C/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "figure.c"
+
+int main(void) {
+    /* TODO */
+    return 0;
+}
