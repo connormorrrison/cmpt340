d1d4e7624cf1e919c3e334067156c3663a8778f5
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-01-12 15:25:17 -0600

    Added main() implementation for sample figures

--- A0/Java/Main.java
+++ A0/Java/Main.java
@@ -1,5 +1,27 @@
 package A0.Java;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+        // Sample figures
+        Figure[] figuresArray = {
+            new Circle(Figure.ORIGIN_X, Figure.ORIGIN_Y, 1),
+            new Rectangle(-3, 2, -1, 3),
+            new Line(1, -2, 45, 1)
+        };
+
+        // Initial figures
+        System.out.println("Figures before transformations:");
+        for (Figure figure : figuresArray) {
+            figure.print();
+        }
+
+        // Figures after transformation xlate by 2, 3 and scale by 2
+        System.out.println("\nFigures after transformations:");
+        for (Figure figure : figuresArray) {
+            figure.xlate(2, 3);
+            figure.scale(2);
+            figure.print();
+        }
+    }
 }
