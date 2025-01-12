a777e384bc405391e87c22f4d245b16f9165083b
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-01-12 06:46:59 -0600

    Add function declarations into Figure.java; design will use inheritance

--- A0/Java/Figure.java
+++ A0/Java/Figure.java
@@ -1,1 +1,7 @@
 package A0.Java;
+
+public abstract class Figure {
+    public abstract void scale(int factor);
+    public abstract void xlate(int xOffset, int yOffset);
+    public abstract void print();
+}
