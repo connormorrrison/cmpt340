90ae59e24d64779d2ec9d4595c4ce6b98694ac32
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-01-27 04:53:13 -0600

    Updated project structure to assignment specifications; created BUILD.sh

---
+++ A1/Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+/Users/connormorrison/Developer/classes-2/cmpt340/tvi340/A1/Rust/target/debug/Rust: /Users/connormorrison/Developer/classes-2/cmpt340/tvi340/A1/Rust/src/main.rs
---
+++ A1/Rust/target/debug/deps/Rust-89ffd8c03a9e2d77.d
@@ -0,0 +1,5 @@
+/Users/connormorrison/Developer/classes-2/cmpt340/tvi340/A1/Rust/target/debug/deps/Rust-89ffd8c03a9e2d77: src/main.rs
+
+/Users/connormorrison/Developer/classes-2/cmpt340/tvi340/A1/Rust/target/debug/deps/Rust-89ffd8c03a9e2d77.d: src/main.rs
+
+src/main.rs:
