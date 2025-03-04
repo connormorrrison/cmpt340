e244279c14ae0aa903eadda525418e5c03577a99
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-03-04 13:40:55 -0600

    Add Cargo.lock, Cargo.toml and src directory

---
+++ A2/Tree/target/debug/Tree.d
@@ -0,0 +1,1 @@
+/Users/connormorrison/Developer/courses/cmpt340/tvi340/A2/Tree/target/debug/Tree: /Users/connormorrison/Developer/courses/cmpt340/tvi340/A2/Tree/src/main.rs
---
+++ A2/Tree/target/debug/deps/Tree-96c446a64369844e.d
@@ -0,0 +1,5 @@
+/Users/connormorrison/Developer/courses/cmpt340/tvi340/A2/Tree/target/debug/deps/Tree-96c446a64369844e: src/main.rs
+
+/Users/connormorrison/Developer/courses/cmpt340/tvi340/A2/Tree/target/debug/deps/Tree-96c446a64369844e.d: src/main.rs
+
+src/main.rs:
