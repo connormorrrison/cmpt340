028f4703077d9d5bd299bcfcf4f15cb12ad66ae0
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-03-09 01:06:55 -0600

    Add imperative approach to main.rs

--- A2/Search/src/main.rs
+++ A2/Search/src/main.rs
@@ -7,7 +7,7 @@ fn binary_search_imperative(vector: &Vec<i32>, target: i32) -> i32 {
     while low <= high {
         let mid = low + (high - low) / 2;
 
-        // Check if target is present at mid
+        // If target is present at mid
         if vector[mid as usize] == target {
             return mid;
         }
@@ -27,7 +27,27 @@ fn binary_search_imperative(vector: &Vec<i32>, target: i32) -> i32 {
 
 // A functional version using recursion and cannot involve
 // mutable variables
-// fn binary_search_functional(vector: &Vec<i32>, target: i32) {}
+fn binary_search_functional(vector: &Vec<i32>, target: i32, low: i32, high: i32) -> i32 {
+    if high >= low {
+        let mid = low + (high - low) / 2;
+
+        // If target is present at mid
+        if vector[mid as usize] == target {
+            return mid;
+        }
+
+        // If target is greater
+        if vector[mid as usize] > target {
+            return binary_search_functional(vector, target, low, mid - 1);
+        }
+
+        // If target is less
+        else {
+            return binary_search_functional(vector, target, mid + 1, high);
+        }
+    }
+    -1
+}
 
 fn main() {
     let sorted_vector = vec![1, 2, 13, 20, 30, 31, 42, 44, 77, 98];
@@ -48,5 +68,23 @@ fn main() {
         println!("Binary search (imperative): target {} was not found in the vector.", target);
     } else {
         println!("Binary search (imperative): target {} found at index {}.", target, result);
+    }
+
+    // Test case where target is present
+    let target = 98;
+    let result = binary_search_functional(&sorted_vector, target, 0, (sorted_vector.len() as i32) - 1);
+    if result == -1 {
+        println!("Binary search (functional): target {} was not found in the vector.", target);
+    } else {
+        println!("Binary search (functional): target {} found at index {}.", target, result);
+    }
+
+    // Test cases where target is not present
+    let target = 3;
+    let result = binary_search_functional(&sorted_vector, target, 0, (sorted_vector.len() as i32) - 1);
+    if result == -1 {
+        println!("Binary search (functional): target {} was not found in the vector.", target);
+    } else {
+        println!("Binary search (functional): target {} found at index {}.", target, result);
     }
 }
