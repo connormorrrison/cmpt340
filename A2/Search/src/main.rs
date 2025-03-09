b84e5a1b1588a4089614c65fa6180134f39e5905
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2025-03-09 00:24:36 -0600

    Add imperative binary search and test cases for present target and not present target

--- A2/Search/src/main.rs
+++ A2/Search/src/main.rs
@@ -1,3 +1,52 @@
+// An imperative version (as described in the book) using
+// a loop and mutable variables
+fn binary_search_imperative(vector: &Vec<i32>, target: i32) -> i32 {
+    let mut low = 0;
+    let mut high = (vector.len() as i32) - 1;
+
+    while low <= high {
+        let mid = low + (high - low) / 2;
+
+        // Check if target is present at mid
+        if vector[mid as usize] == target {
+            return mid;
+        }
+
+        // If target is greater
+        if vector[mid as usize] < target {
+            low = mid + 1;
+        }
+
+        // If target is smaller
+        else {
+            high =  mid - 1;
+        }
+    }
+    -1
+}
+
+// A functional version using recursion and cannot involve
+// mutable variables
+// fn binary_search_functional(vector: &Vec<i32>, target: i32) {}
+
 fn main() {
-    println!("Hello, planet!");
+    let sorted_vector = vec![1, 2, 13, 20, 30, 31, 42, 44, 77, 98];
+
+    // Test case where target is present
+    let target = 77;
+    let result = binary_search_imperative(&sorted_vector, target);
+    if result == -1 {
+        println!("Binary search (imperative): target {} was not found in the vector.", target);
+    } else {
+        println!("Binary search (imperative): target {} found at index {}.", target, result);
+    }
+
+    // Test cases where target is not present
+    let target = 15;
+    let result = binary_search_imperative(&sorted_vector, target);
+    if result == -1 {
+        println!("Binary search (imperative): target {} was not found in the vector.", target);
+    } else {
+        println!("Binary search (imperative): target {} found at index {}.", target, result);
+    }
 }
---
+++ A2/Search/target/debug/deps/Search-33ee48291a60947d.d
@@ -0,0 +1,5 @@
+/Users/connormorrison/Developer/courses/cmpt340/tvi340/A2/Search/target/debug/deps/Search-33ee48291a60947d: src/main.rs
+
+/Users/connormorrison/Developer/courses/cmpt340/tvi340/A2/Search/target/debug/deps/Search-33ee48291a60947d.d: src/main.rs
+
+src/main.rs:
